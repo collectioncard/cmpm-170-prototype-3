e5fec72c33eba55cc5445ade8cc8629325388857
Thomas Wessel <turpleturtle12@gmail.com>
2024-11-25 14:51:44 -0800

    Updates from meeting

--- prototype-4--bird-game/scenes/player.gd
+++ prototype-4--bird-game/scenes/player.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 700.0
-const JUMP_VELOCITY = -900.0
+const JUMP_VELOCITY = -700.0
 
+@onready var cur_bird : CharacterBody2D = get_node("bird");
+
+func _process(_delta: float) -> void:
+	if position.y > 1500:
+		position = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,8 +16,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		if (position.distance_to(cur_bird.global_position) < 75):	
+			velocity.y = JUMP_VELOCITY - 100
+		else:
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- prototype-4--bird-game/scripts/bird.gd
+++ prototype-4--bird-game/scripts/bird.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 500.0
 const ATTACK_SPEED_MUX = 3.0  # X times faster than normal flying speed
 const ATTACKING_DURATION = 150  # msec
-const RANGE = 100  # how far the bird can be away from the player
+const ATTACKING_COOLDOWN = 500;
+const RANGE = 300  # how far the bird can be away from the player
 
+@onready var anims : AnimatedSprite2D = get_node("AnimatedSprite2D");
 
 var attacking: bool = false
+var attack_dir : int = 0;
+var attacking_cooldown_time = -1
 var attacking_start_time: float = 0.0
 
 func attack_flying():
-	var direction = Vector2(1, 1).normalized()
+	anims.play("attacking");
+	var direction = Vector2(attack_dir, 1).normalized()
 	velocity = direction * SPEED * ATTACK_SPEED_MUX
 
 
 func normal_flying():
 	# read mouse current location
+	anims.play("fly");
 	var mouse_position = get_global_mouse_position()
 
-	var player_position = Vector2(100, 100)  # HACK
+	var player_position = get_parent().position + Vector2.UP * 50;  # TODO: Player POS CHANGE
 
 
-	var target_position  # where the bird is flying toward
+	var target_position : Vector2  # where the bird is flying toward
 
 	if player_position.distance_to(mouse_position) > RANGE:
 		var heading = (mouse_position - player_position).normalized()
@@ -34,24 +40,35 @@ func normal_flying():
 
 	# move the bird
 	var direction = (target_position - global_position).normalized()
-	velocity = direction * SPEED
+	if (target_position.distance_to(global_position) < 40):
+		velocity = Vector2.ZERO
+	else:
+		velocity = direction * SPEED
 
+func _process(_delta: float) -> void:
+	rotation = abs(velocity.angle())
+	anims.flip_v = (rotation) > PI/2;
+	
+	if (global_position.distance_to(get_parent().position) > 1000):
+		position = Vector2.ZERO
 
 func _physics_process(_delta: float) -> void:
-	if (Time.get_ticks_msec() - attacking_start_time) > ATTACKING_DURATION:
+	if attacking and (Time.get_ticks_msec() - attacking_start_time) > ATTACKING_DURATION:
 		attacking = false
-
+	attacking_cooldown_time -= _delta * 1000;
 	if attacking:
 		attack_flying()
 	else:
 		normal_flying()
 
-	print(velocity)  # HACK
+	#print(velocity)  # HACK
 	move_and_slide()
 
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
+		if event.button_index == MOUSE_BUTTON_LEFT and attacking_cooldown_time < 0:
+			attacking_cooldown_time = ATTACKING_COOLDOWN + ATTACKING_DURATION
 			attacking = true
+			attack_dir = sign(get_global_mouse_position().x - global_position.x )
 			attacking_start_time = Time.get_ticks_msec()
